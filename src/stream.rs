eb870c0e7fac1a5a1295c0de92bcc24d5cc8bf9b
watcol <potfman@gmail.com>
2021-12-29 17:05:19 +0900

    Add TODOs

--- src/stream.rs
+++ src/stream.rs
@@ -4,6 +4,17 @@
 //! sometimes [`Rewind`] will be required), so here we'll provide some implementations
 //! on them by wrapping types implementing [`Stream`], [`AsyncRead`], etc.
 //!
+//! ## TODO
+//! - [ ] InfallibleStream (Stream -> TryStream)
+//! - [ ] IteratorStream (Iterator -> Stream)
+//! - [ ] PositionedStream (TryStream -> TryStream + Positioned)
+//! - [ ] RecordedStream (TryStream -> TryStream + Rewind) (saving output with `Vec`, using it
+//! to `Rewind`).
+//! - [ ] ExtendStream (TryStream -> TryStream) (saving output with `Extend`)
+//! - [ ] BufferedStream (TryStream -> TryStream + Rewind)
+//! - [ ] ReaderStream (AsyncRead -> TryStream)
+//! - [ ] SeekStream (TryStream + AsyncSeek -> TryStream + Rewind)
+//!
 //! [`Rewind`]: ./trait.Rewind.html
 //! [`Positioned`]: ./trait.Positioned.html
 //! [`Stream`]: https://docs.rs/futures/latest/futures/stream/trait.Stream.html
