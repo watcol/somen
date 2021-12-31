cae52b5f006d538b0747f9de5ad9e0eac31175d5
watcol <potfman@gmail.com>
2021-12-31 15:13:49 +0900

    changed the project name

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 //! An asynchronous parser combinator library.
-//!
-//! flumen (/ˈfluː.men/, "river" or "stream" in Latin) is an asynchronous parser combinator.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
