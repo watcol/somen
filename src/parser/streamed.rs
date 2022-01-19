202dfce78ab22494f69b66c70f95532d62c2771b
watcol <potfman@gmail.com>
2022-01-19 22:34:04 +0900

    Improved documents for `StreamedParser`.

--- src/parser/streamed.rs
+++ src/parser/streamed.rs
@@ -1,7 +1,9 @@
+//! Tools for parsers return multiple outputs.
+
 use crate::stream::BasicInput;
 use futures_core::stream::TryStream;
 
-/// A trait for parsers returning multiple outputs with [`Stream`].
+/// A trait for parsers return multiple outputs with [`Stream`].
 ///
 /// [`Stream`]: futures_core::stream::Stream
 pub trait StreamedParser<I: BasicInput + ?Sized> {
@@ -15,5 +17,7 @@ pub trait StreamedParser<I: BasicInput + ?Sized> {
     type Stream: TryStream<Ok = Self::Output, Error = Self::Error>;
 
     /// Takes an input, returns multiple outputs with [`Stream`].
+    ///
+    /// [`Stream`]: futures_core::stream::Stream
     fn parser_stream(&self, input: &mut I) -> Self::Stream;
 }
