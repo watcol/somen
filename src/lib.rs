b99422f822f5fd675c2d04ba180392ec390a8d98
watcol <potfman@gmail.com>
2022-01-23 17:05:24 +0900

    Add module "prelude".

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,20 @@ extern crate alloc;
 pub mod error;
 pub mod parser;
 pub mod stream;
+
+/// Re-exports of commonly used items.
+pub mod prelude {
+    #[doc(no_inline)]
+    pub use crate::parser::streamed::StreamedParserExt as _;
+    #[doc(no_inline)]
+    pub use crate::parser::ParserExt as _;
+    #[doc(no_inline)]
+    pub use crate::stream::StreamBuilder as _;
+
+    #[doc(inline)]
+    pub use crate::parser::streamed::StreamedParser;
+    #[doc(inline)]
+    pub use crate::parser::{any, Parser};
+    #[doc(inline)]
+    pub use crate::stream::{self, Input, Positioned};
+}
