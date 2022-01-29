767a666fbb0aa10e5a47e89a559903fbd5012f65
watcol <potfman@gmail.com>
2022-01-29 22:25:30 +0900

    Add prelude.

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,6 @@ pub mod prelude {
     pub use crate::stream::StreamBuilder as _;
 
     pub use crate::parser::streamed::StreamedParser;
-    pub use crate::parser::{any, function, is, value, Parser};
+    pub use crate::parser::{any, eof, function, is, token, value, Parser};
     pub use crate::stream::{self, Input, Positioned};
 }
