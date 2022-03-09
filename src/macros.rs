19db41671d1d14eb288b2a184851fa53fa5f3bd4
watcol <potfman@gmail.com>
2022-03-09 15:02:55 +0900

    Add methods

--- src/macros.rs
+++ src/macros.rs
@@ -30,7 +30,7 @@ macro_rules! parser_state {
             $(, $Ps: $trait<$I>)*
             $($(, $Ts)*)?
         > {
-            $($field : $crate::parser_state_inner!{@type; $($([$k $(= $v)?])*)? $ty}),*
+            $($field : $crate::parser_state_inner!{@type; $($({$k $(= $v)?})*)? $ty}),*
         }
 
         impl <
@@ -45,9 +45,21 @@ macro_rules! parser_state {
             #[inline]
             fn default() -> Self {
                 Self {
-                    $($field: $crate::parser_state_inner!{@default; $($([$k $(= $v)?])*)? $ty}),*
+                    $($field: $crate::parser_state_inner!{@default; $($({$k $(= $v)?})*)? $ty}),*
                 }
             }
+        }
+
+        impl <
+            $I: $crate::stream::Positioned $(+ $Itrait)? + ?Sized
+            $(, $Ps: $trait<$I>)*
+            $($(, $Ts $(: core::default::$def)?)*)?
+        > $name <
+            $I
+            $(, $Ps)*
+            $($(, $Ts)*)?
+        > {
+            $($crate::parser_state_inner!{@impl; $($({$k $(= $v)?})*)? $field: $ty})*
         }
     }
 }
@@ -58,31 +70,124 @@ macro_rules! parser_state_inner {
     (@type; $ty:ty) => {
         $ty
     };
-    (@type; [option]$([$k_rest:ident $(= $v_rest:ident)?])* $ty:ty) => {
+    (@type; {option}$({$k_rest:ident $(= $v_rest:ident)?})* $ty:ty) => {
         core::option::Option<$ty>
     };
-    (@type; [$k:ident $(= $v:ident)?]$([$k_rest:ident $(= $v_rest:ident)?])* $ty:ty) => {
-        $crate::parser_state_inner!(@type; $([$k_rest $(= $v_rest)?])* $ty)
+    (@type; {$k:ident $(= $v:ident)?}$({$k_rest:ident $(= $v_rest:ident)?})* $ty:ty) => {
+        $crate::parser_state_inner!(@type; $({$k_rest $(= $v_rest)?})* $ty)
     };
-    (@default; <opt> $ty:ty) => {
+    (@default; .opt $ty:ty) => {
         core::option::Option::None
     };
     (@default; $ty:ty) => {
         core::default::Default::default()
     };
     (@default;
-        [default = $func:ident]$([$k_rest:ident $(= $v_rest:ident)?])* $(<opt>)? $ty:ty
+       $(.opt)? {default = $func:ident}$({$k_rest:ident $(= $v_rest:ident)?})* $ty:ty
     ) => {
         $func()
     };
     (@default;
-        [option]$([$k_rest:ident $(= $v_rest:ident)?])* $(<opt>)? $ty:ty
+        $(.opt)? {option}$({$k_rest:ident $(= $v_rest:ident)?})* $ty:ty
     ) => {
-        $crate::parser_state_inner!{@default; $([$k_rest $(= $v_rest)?])* <opt> $ty}
+        $crate::parser_state_inner!{@default; .opt $({$k_rest $(= $v_rest)?})* $ty}
     };
     (@default;
-        [$k:ident $(= $v:ident)?]$([$k_rest:ident $(= $v_rest:ident)?])* $(<$opt:ident>)? $ty:ty
+        $(.$opt:ident)? {$k:ident $(= $v:ident)?}$({$k_rest:ident $(= $v_rest:ident)?})* $ty:ty
     ) => {
-        $crate::parser_state_inner!{@default; $([$k_rest = $v_rest])* $(<$opt>)? $ty}
+        $crate::parser_state_inner!{@default; $(.$opt)? $({$k_rest = $v_rest})* $ty}
+    };
+    (@impl;
+        <opt = opt> $(<set = $set:ident>)? $(<get = $get:ident>)? $(<take = $take:ident>)?
+        $field:ident : $ty:ty
+    ) => {
+        $(
+            #[inline]
+            fn $set<F: Fn() -> $ty>(&mut self, f: F) {
+                if self.$field.is_none() {
+                    self.$field = Some(f());
+                }
+            }
+        )?
+        $(
+            #[inline]
+            fn $get(&self) -> $ty {
+                self.$field.clone().unwrap()
+            }
+        )?
+        $(
+            #[inline]
+            fn $take(&mut self) -> $ty {
+                core::mem::take(&mut self.$field).unwrap()
+            }
+        )?
+    };
+    (@impl;
+        $(<set = $set:ident>)? $(<get = $get:ident>)? $(<take = $take:ident>)?
+        $field:ident : $ty:ty
+    ) => {
+        $(
+            #[inline]
+            fn $set<F: Fn() -> $ty>(&mut self, f: F) {
+                self.$field = f();
+            }
+        )?
+        $(
+            #[inline]
+            fn $get(&self) -> $ty {
+                self.$field.clone()
+            }
+        )?
+        $(
+            #[inline]
+            fn $take(&mut self) -> $ty {
+                core::mem::take(&mut self.$field)
+            }
+        )?
+    };
+    (@impl;
+        $(<opt = $_:ident>)? $(<set = $set:ident>)? $(<get = $get:ident>)? $(<take = $take:ident>)?
+        {option}$({$k_rest:ident $(= $v_rest:ident)?})* $field:ident : $ty:ty
+    ) => {
+        $crate::parser_state_inner!{@impl;
+            <opt = opt> $(<set = $set>)? $(<get = $get>)? $(<take = $take>)?
+            $({$k_rest $(= $v_rest)?})* $field: $ty
+        }
+    };
+    (@impl;
+        $(<opt = $opt:ident>)? $(<set = $_:ident>)? $(<get = $get:ident>)? $(<take = $take:ident>)?
+        {set = $set:ident}$({$k_rest:ident $(= $v_rest:ident)?})* $field:ident : $ty:ty
+    ) => {
+        $crate::parser_state_inner!{@impl;
+            $(<opt = $opt>)? <set = $set> $(<get = $get>)? $(<take = $take>)?
+            $({$k_rest $(= $v_rest)?})* $field: $ty
+        }
+    };
+    (@impl;
+        $(<opt = $opt:ident>)? $(<set = $set:ident>)? $(<get = $_:ident>)? $(<take = $take:ident>)?
+        {get = $get:ident}$({$k_rest:ident $(= $v_rest:ident)?})* $field:ident : $ty:ty
+    ) => {
+        $crate::parser_state_inner!{@impl;
+            $(<opt = $opt>)? $(<set = $set>)? <get = $get> $(<take = $take>)?
+            $({$k_rest $(= $v_rest)?})* $field: $ty
+        }
+    };
+    (@impl;
+        $(<opt = $opt:ident>)? $(<set = $set:ident>)? $(<get = $get:ident>)? $(<take = $_:ident>)?
+        {take = $take:ident}$({$k_rest:ident $(= $v_rest:ident)?})* $field:ident : $ty:ty
+    ) => {
+        $crate::parser_state_inner!{@impl;
+            $(<opt = $opt>)? $(<set = $set>)? $(<get = $get>)? <take = $take>
+            $({$k_rest $(= $v_rest)?})* $field: $ty
+        }
+    };
+    (@impl;
+        $(<opt = $opt:ident>)? $(<set = $set:ident>)? $(<get = $get:ident>)? $(<take = $take:ident>)?
+        {$k:ident $(= $v:ident)?}$({$k_rest:ident $(= $v_rest:ident)?})* $field:ident : $ty:ty
+    ) => {
+        $crate::parser_state_inner!{@impl;
+            $(<opt = $opt>)? $(<set = $set>)? $(<get = $get>)? $(<take = $take>)?
+            $({$k_rest $(= $v_rest)?})* $field: $ty
+        }
     };
 }
